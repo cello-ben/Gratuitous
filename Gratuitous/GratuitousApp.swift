ce8dd8abe21b8b1c9321dfb347af663437e61d79
MacMania Networks, LLC <mail@celloben.com>
2023-06-12 14:21:07 -0500

    Initial Commit

---
+++ Gratuitous/GratuitousApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratuitousApp.swift
+//  Gratuitous
+//
+//  Created by Cello Ben on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GratuitousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
